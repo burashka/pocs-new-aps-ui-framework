f0e315dcc9745904ca06a46801d73ef3492fb2ba
burashka <burashka-lcme@yandex.ru>
2018-05-31 18:57:44 +0300

    Create micro-components

--- React/pure/src/ui/UserView.js
+++ React/pure/src/ui/UserView.js
@@ -58,6 +58,77 @@ function calculateDiff(invitationDate){
 	return Math.ceil(timeDiff / (1000 * 3600));
 }
 
+function MessageList({ disabled, locked, status, invitationDate}){
+	return <React.Fragment>
+		{ disabled &&
+			<Message bsStyle = "warning">
+				{ _("The user has been disabled. He/she is not able to log in to Control Panel or use assigned services.") }
+			</Message> }
+		{ locked &&
+			<Message bsStyle = "warning">
+				{ _("The user has exceeded amount of allowed login attempts and got locked. Click Unlock to enable login back.") }
+			</Message>}
+		{ status === "invited" &&
+			<Message bsStyle = "warning">
+				{ calculateDiff(invitationDate) > 48
+					? _("The user did not complete the activation. You need to resend an invitation, because the activation link has expired.")
+					: _("The user did not yet complete the activation. The activation link emailed to the user, expires in 48 hours. If needed, you can resend an invitation.") }
+			</Message> }
+	</React.Fragment>;
+}
+
+function Toolbar({ hasManagePrivilege, hasLoginAsPermission, isCurrentUser, btnStatus, givenName, isAdmin }) {
+	return <React.Fragment>
+		{ hasManagePrivilege
+			&& hasLoginAsPermission
+			&& !isCurrentUser
+			&& btnStatus
+			&& <Button
+				bsStyle = "primary"
+				label = {_(`Login as ${givenName }`)}
+			/>
+		}
+		{ hasManagePrivilege
+			&& !isCurrentUser
+			&& btnStatus
+			&& <Button
+				bsStyle = "primary"
+				label = {_("Reset password")}
+			/>
+		}
+		{ (isCurrentUser && !invitationDate) && <Button
+			bsStyle = "primary"
+			label = {_("Change Password")}
+			/>
+		}
+		{ (!isAdmin || !isCurrentUser) && <Button
+			bsStyle = "danger"
+			label = {_("Delete")}
+		/> }
+	</React.Fragment>
+}
+
+function UserSettings({ isAdmin, user: { fullName, telWork, telCell, login, email, notificationEmail } }) {
+	const isEmailHidden = login === email && (!notificationEmail || notificationEmail === email);
+	const isNotificationEmailHidden = isAdmin || !notificationEmail || (notificationEmail === email && notificationEmail === login);
+
+	return <Panel
+		title = { _("User Settings") }
+	>
+		<Output label={_("Name")} value={fullName} />
+		<Output label={_("Role")} value={isAdmin ? _("Admin") : _("User")} />
+		<Output label={_("Phone")} value={telWork} />
+		<Output label={_("Mobile Phone")} value={telCell} />
+		<Output label={_("Language")} value={_("English (United States)")} />
+		<Output
+			label={ isEmailHidden && isNotificationEmailHidden ? _("Email") : _("Email (To Sign In)")}
+			value={login}
+		/>
+		{!isEmailHidden && <Output label={_("Personal Email")} value={email} />}
+		{!isNotificationEmailHidden && <Output label={_("Notification Email")} value={notificationEmail} />}
+	</Panel>;
+}
+
 class UserView extends Component {
 	constructor(props) {
 		super(props);
@@ -81,76 +152,39 @@ class UserView extends Component {
 
 	render() {
 		const { vars: {selectedUser}, user } = this.props.context;
-		const { disabled, locked, invitationDate, telWork, telCell, login, email, notificationEmail, fullName, addressPostal, givenName } = selectedUser;
+		const { disabled, locked, invitationDate, addressPostal, givenName } = selectedUser;
 		const isAdmin = selectedUser.aps.type.includes("http://parallels.com/aps/types/pa/admin-user");
 		const isCurrentUser = user.aps.id === selectedUser.aps.id;
-		const isEmailHidden = login === email && (!notificationEmail || notificationEmail === email);
-		const isNotificationEmailHidden = isAdmin || !notificationEmail || (notificationEmail === email && notificationEmail === login);
 		const status = getStatus(selectedUser);
-		const hasLoginAsPermission = checkPrivilege(`http://www.parallels.com/pa/pa-core-services#${isAdmin ? "own_users-manage": "mycp_access"}`);
-		const hasManagePrivilege = !user.isAccountAdmin || checkPrivilege("http://www.parallels.com/pa/pa-core-services#own_account-manage");
 		const loginTime = this.state.loginTime;
 
 		return <React.Fragment>
-			{ (disabled || locked) && <Message bsStyle = "warning">
-				{ disabled
-					? _("The user has been disabled. He/she is not able to log in to Control Panel or use assigned services.")
-					: _("The user has exceeded amount of allowed login attempts and got locked. Click Unlock to enable login back.") }
-			</Message> }
-			{ status === "invited" &&
-				<Message bsStyle = "warning">
-				{ calculateDiff(invitationDate) > 48
-					? _("The user did not complete the activation. You need to resend an invitation, because the activation link has expired.")
-					: _("The user did not yet complete the activation. The activation link emailed to the user, expires in 48 hours. If needed, you can resend an invitation.") }
-			</Message> }
-			{ hasManagePrivilege
-				&& hasLoginAsPermission
-				&& !isCurrentUser
-				&& ["ready", "updating", "locked", "disabled"].includes(status)
-				&& <Button
-					bsStyle = "primary"
-					label = {_(`Login as ${givenName }`)}
-				/>
-			}
-			{ hasManagePrivilege
-				&& !isCurrentUser
-				&& ["ready", "updating", "locked", "disabled"].includes(status)
-				&& <Button
-					bsStyle = "primary"
-					label = {_("Reset password")}
-				/>
-			}
-			{ (isCurrentUser && !invitationDate) && <Button
-				bsStyle = "primary"
-				label = {_("Change Password")}
-			/> }
-			{ (!isAdmin || !isCurrentUser) && <Button
-				bsStyle = "danger"
-				label = {_("Delete")}
-			/>}
-			<Panel
-				title = { _("User Settings") }
-			>
-				<Output label={_("Name")} value={fullName} />
-				<Output label={_("Role")} value={isAdmin ? _("Admin") : _("User")} />
-				<Output label={_("Phone")} value={telWork} />
-				<Output label={_("Mobile Phone")} value={telCell} />
-				<Output label={_("Language")} value={_("English (United States)")} />
-				<Output
-					label={ isEmailHidden && isNotificationEmailHidden ? _("Email") : _("Email (To Sign In)")}
-					value={login}
-				/>
-				{!isEmailHidden && <Output label={_("Personal Email")} value={email} />}
-				{!isNotificationEmailHidden && <Output label={_("Notification Email")} value={notificationEmail} />}
-			</Panel>
-			<Panel
-				title = { _("Postal Address") }
-			>
+			<MessageList
+				disabled = { disabled }
+				locked = {locked}
+				status = {status}
+				invitationDate = {invitationDate}
+			/>
+			<Toolbar
+				hasManagePrivilege = {
+					!user.isAccountAdmin || checkPrivilege("http://www.parallels.com/pa/pa-core-services#own_account-manage")
+				}
+				hasLoginAsPermission = {
+					checkPrivilege(`http://www.parallels.com/pa/pa-core-services#${isAdmin ? "own_users-manage": "mycp_access"}`)
+				}
+				isCurrentUser = { isCurrentUser }
+				btnStatus = { ["ready", "updating", "locked", "disabled"].includes(status) }
+				givenName = { givenName }
+				isAdmin = { isAdmin }
+			/>
+			<UserSettings
+				isAdmin = { isAdmin }
+				user = { selectedUser }
+			/>
+			<Panel title = { _("Postal Address") }>
 				<Output value={`${addressPostal.locality}, ${addressPostal.extendedAddress}`} />
 			</Panel>
-			<Panel
-				title = { _("Login History") }
-			>
+			<Panel title = { _("Login History") }>
 				<Output
 					label={ loginTime && _("Last accessed")}
 					value={ loginTime ? loginTime : (isCurrentUser ? _("It’s the first time you logged in.") : _("This user has never logged in.")) }
